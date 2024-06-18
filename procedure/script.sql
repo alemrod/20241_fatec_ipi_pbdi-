8710f3db8d28db6fdf652a9e2300e4e85f17b2f3
Alexandre <alemrod01@gmail.com>
2024-06-17 23:11:17 -0300

    feat(ex11): entrega 1.3

--- procedure/script.sql
+++ procedure/script.sql
@@ -1,17 +1,14 @@
---1.2 Procedimento para contar pedidos de um cliente com RAISE NOTICE
-CREATE OR REPLACE PROCEDURE sp_contar_pedidos_cliente(
-    IN p_cod_cliente INT
+-- 1.3 Procedimento para contar pedidos com variável de saída
+CREATE OR REPLACE PROCEDURE sp_contar_pedidos_cliente_out(
+    IN p_cod_cliente INT,
+    OUT p_total_pedidos INT
 ) LANGUAGE plpgsql AS $$
-DECLARE
-    v_total_pedidos INT;
 BEGIN
-    SELECT COUNT(*) INTO v_total_pedidos
+    SELECT COUNT(*) INTO p_total_pedidos
     FROM pedido
     WHERE cod_cliente = p_cod_cliente;
 
-    RAISE NOTICE 'Total de pedidos do cliente %: %', p_cod_cliente, v_total_pedidos;
-
     -- Registrar log
-    INSERT INTO log_operacoes (nome_procedimento) VALUES ('sp_contar_pedidos_cliente');
+    INSERT INTO log_operacoes (nome_procedimento) VALUES ('sp_contar_pedidos_cliente_out');
 END;
 $$;
