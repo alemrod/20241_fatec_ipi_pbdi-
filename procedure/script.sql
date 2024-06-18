bf14176c256d133edd3baee87a29a200655ecf16
Alexandre <alemrod01@gmail.com>
2024-06-17 23:11:57 -0300

    feat(ex11): entrega 1.4

--- procedure/script.sql
+++ procedure/script.sql
@@ -1,14 +1,13 @@
--- 1.3 Procedimento para contar pedidos com variável de saída
-CREATE OR REPLACE PROCEDURE sp_contar_pedidos_cliente_out(
-    IN p_cod_cliente INT,
-    OUT p_total_pedidos INT
+-- 1.4 Procedimento com parâmetro INOUT
+CREATE OR REPLACE PROCEDURE sp_contar_pedidos_cliente_inout(
+    INOUT p_cod_cliente INT
 ) LANGUAGE plpgsql AS $$
 BEGIN
-    SELECT COUNT(*) INTO p_total_pedidos
+    SELECT COUNT(*) INTO p_cod_cliente
     FROM pedido
     WHERE cod_cliente = p_cod_cliente;
 
     -- Registrar log
-    INSERT INTO log_operacoes (nome_procedimento) VALUES ('sp_contar_pedidos_cliente_out');
+    INSERT INTO log_operacoes (nome_procedimento) VALUES ('sp_contar_pedidos_cliente_inout');
 END;
 $$;
